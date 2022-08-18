34031e997b569fd77d72744be936ec10b2d3afcc
Haimanjyot Singh <haimanjyot.m@gmail.com>
2022-08-18 07:59:43 +0530

    Add files via upload

---
+++ conditional_programming/electric_bill_calculator.py
@@ -0,0 +1,17 @@
+units = eval(input('Please enter the number of units consumed --- '))
+
+while units < 0:
+    print('The units can\'t be less than zero.')
+    units = eval(input('Please enter the number of units consumed --- '))
+
+def generator(x):
+    if x <= 100:
+        return x*0.8
+    elif x <= 250:
+        return 80+(x-100)
+    elif x <= 400:
+        return 230+((x-250)*1.2)
+    else:
+        return 410+((x-400)*1.5)
+
+print(f'The bill generated is {generator(units)}.')
