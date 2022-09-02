3ec9d2e0d69ba77e933d36d7858123830e11cfda
Haimanjyot Singh <haimanjyot.m@gmail.com>
2022-09-02 19:32:47 +0530

    Updated dad_jokes.py

--- extra/dad_jokes.py
+++ extra/dad_jokes.py
@@ -9,18 +9,21 @@ query = input("Please enter a search term --- ")
 res = requests.get(
     url="https://icanhazdadjoke.com/search",
     params={"term": query},
-    headers={"accept": "text/plain"},
+    headers={"accept": "application/json"},
 )
 
-if res.text:
-    print(f'\nHere are all the dad jokes I could find based on "{query}":')
-    print(res.text)
-else:
-    print(f'\nI could not find a joke based on "{query}" so here is a random joke:')
+jokes = res.json()["results"]
+total = res.json()["total_jokes"]
+
+if total == 0:
+    print(f'I could not find a joke based on "{query}", so here is a random joke:\n')
     print(
-        (
-            requests.get(
-                url="https://icanhazdadjoke.com", headers={"accept": "text/plain"}
-            )
+        requests.get(
+            url="https://icanhazdadjoke.com",
+            headers={"accept": "text/plain"},
         ).text
     )
+else:
+    print(f'I could find {total} jokes based on "{query}":\n')
+    for i in range(total):
+        print(f'{i+1}. {res.json()["results"][i]["joke"]}')
