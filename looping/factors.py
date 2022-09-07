33eb8861d02b7875c409b6bd7b32ff12ec8e7016
Haimanjyot Singh <haimanjyot.m@gmail.com>
2022-09-07 11:13:04 +0530

    Created factors.py

---
+++ looping/factors.py
@@ -0,0 +1,18 @@
+num = int(input('Please enter a number to get its factors --- '))
+
+factors = []
+
+if num < 0:
+    num *= -1
+    for i in range(1, num+1):
+        if num%i == 0:
+            factors.append(i)
+            factors.append(i*-1)
+else:
+    for i in range(1, num+1):
+        if num%i == 0:
+            factors.append(i)
+
+print(f'The factors of {num} are:')
+for i in range(len(factors)):
+    print(f'{i+1}) {factors[i]}')
