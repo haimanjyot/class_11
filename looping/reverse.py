213105876f861164098ad14e9722885d6ab6c6a9
Haimanjyot Singh <haimanjyot.m@gmail.com>
2022-09-07 10:11:11 +0530

    Updated reverse.py

--- looping/reverse.py
+++ looping/reverse.py
@@ -1,32 +1,28 @@
 # Wap to input a number and display the reverse of the number.
 
 num = int(input('Please enter a number to get its reverse --- '))
-# num = str(num)
 
-# final = ''
+num = str(num)
 
-# for i in range(1, len(num)+1):
-#     final += num[-1*i]
+if num[0] == '-':
+    num = num[1::]
+    print(f'-{num[::-1]}')
+else:
+    print(num[::-1])
 
-# if final[-1] == '-':
-#     final = final[:-1]
-#     final = '-' + final
+# final = 0
+
+# if num < 0:
+#     num = num*-1
+
+#     for i in range(len(str(num))):
+#         final += (num%10)*(10**(len(str(num))-1))
+#         num = num//10
+
+#     final = final*-1
+# else:
+#     for i in range(len(str(num))):
+#         final += (num%10)*(10**(len(str(num))-1))
+#         num = num//10
 
 # print(final)
-
-final = 0
-
-if num < 0:
-    num = num*-1
-
-    for i in range(len(str(num))):
-        final += (num%10)*(10**(len(str(num))-1))
-        num = num//10
-
-    final = final*-1
-else:
-    for i in range(len(str(num))):
-        final += (num%10)*(10**(len(str(num))-1))
-        num = num//10
-
-print(final)
