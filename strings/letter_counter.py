1d375dd46be341c6a4d354e1150e44df141fe778
Haimanjyot Singh <haimanjyot.m@gmail.com>
2022-10-07 19:51:50 +0530

    Added instructions to files.

--- strings/letter_counter.py
+++ strings/letter_counter.py
@@ -1,1 +1,1 @@
-
+# WAP to input a sentence from the user and tell how many 'a's there are.
--- strings/name_functions.py
+++ strings/name_functions.py
@@ -1,1 +1,6 @@
-
+"""
+WAP to input the complete name and do the following (ex. - Mohan Lal Avasthi):
+a) Display the name in initials (M.L.A.)
+b) Display the short name (M.L. Avasthi)
+c) Display the name in Bibliography format (Avasthi, M.L.)
+"""
