8bc76609cceeb2fa38aae1b499ca015481a38401
Haimanjyot Singh <haimanjyot.m@gmail.com>
2022-10-10 08:24:39 +0530

    Edited word_counter

--- strings/word_counter.py
+++ strings/word_counter.py
@@ -1,7 +1,14 @@
 # WAP to input a sentence from the user and tell how many words there are.
 
+from curses.ascii import isalpha
+
+
 sen = input('Please enter a sentence --- ')
 
 words = sen.split(' ')
+final = 0
+for i in words:
+    if i.isalpha():
+        final += 1
 
-print(f'There are/is {len(words)} word(s) in the sentence.')
+print(f'There are/is {final} word(s) in the sentence.')
