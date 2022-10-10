75a52c504bcd109fcc8df0cb59f70d42d5eb0912
Haimanjyot Singh <haimanjyot.m@gmail.com>
2022-10-10 08:25:31 +0530

    Edited word_counter

--- strings/word_counter.py
+++ strings/word_counter.py
@@ -1,8 +1,5 @@
 # WAP to input a sentence from the user and tell how many words there are.
 
-from curses.ascii import isalpha
-
-
 sen = input('Please enter a sentence --- ')
 
 words = sen.split(' ')
