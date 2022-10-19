902c5359e9727892714327a015f4bb208040d06e
Haimanjyot Singh <haimanjyot.m@gmail.com>
2022-10-19 17:35:46 +0530

    Edited capitalize.py

--- strings/capitalize.py
+++ strings/capitalize.py
@@ -1,1 +1,17 @@
 # WAP to input a sentence and capitalize the first letter of each word in it.
+
+sen = input('Please enter a sentence --- ')
+seperated = sen.split()
+final = ''
+
+for i in seperated:
+    final += f'{i.capitalize()} '
+
+final = final[:-1]
+
+print(final)
+
+'''
+Method using .capwords():
+sen = sen.capwords()
+'''
